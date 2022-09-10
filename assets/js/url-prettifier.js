29c2e1c2a5f2dff8708b4f510f35d48b6521164a
Henrik Christensen <sensen1695@hotmail.com>
2022-09-10 19:35:35 +0200

    dropdown nav menu now collapses when you tap a link

--- assets/js/url-prettifier.js
+++ assets/js/url-prettifier.js
@@ -1,8 +1,13 @@
+const burgerMenu = document.querySelector('#navbarSupportedContent');
 const links = document.querySelectorAll('.link');
 
 links.forEach(link => {
     link.addEventListener('click', e => {
         e.preventDefault();
+
+        if (burgerMenu.classList.contains('show')) {
+            burgerMenu.classList.remove('show');
+        }
 
         const href = link.getAttribute('href');
         const id = href.substring(1);
